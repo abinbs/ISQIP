072f87ed25095f528e916401d3d0bb91c7f1c4d2
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-10-03 19:33:05 +0400

    made simple changes

--- consecutive_num_square.py
+++ consecutive_num_square.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
-    n = int(input())
-    for i in range(0,n):
-        print(i*i)
+    n = int(input("Enter the number:"))
+    for i in range(0,n,1):
+        a=0
+        a=i*i
+        print(a)
